75873e689bf0ff9cf5038a55846005a9a6b177c4
201205964 <anthonymartinez27331@gmail.com>
2023-05-25 11:54:27 -0700

    Main Activity completed

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -6,15 +6,48 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnSound1;
+    private Button btnSound11;
+    private Button btnSound12;
+    private Button btnSound13;
+    private Button btnSound14;
+    private Button btnSound21;
+    private Button btnSound22;
+    private Button btnSound23;
+    private Button btnSound24;
+    private Button btnSound31;
+    private Button btnSound32;
+    private Button btnSound33;
+    private Button btnSound34;
+    private Button btnSound41;
+    private Button btnSound42;
+    private Button btnSound43;
+    private Button btnSound44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSound1 = findViewById(R.id.button1);
+        btnSound11 = findViewById(R.id.button11);
+        btnSound12 = findViewById(R.id.button12);
+        btnSound13 = findViewById(R.id.button13);
+        btnSound14 = findViewById(R.id.button14);
+        btnSound21 = findViewById(R.id.button21);
+        btnSound22 = findViewById(R.id.button22);
+        btnSound23 = findViewById(R.id.button23);
+        btnSound24 = findViewById(R.id.button24);
+        btnSound31 = findViewById(R.id.button31);
+        btnSound32 = findViewById(R.id.button32);
+        btnSound33 = findViewById(R.id.button33);
+        btnSound34 = findViewById(R.id.button34);
+        btnSound41 = findViewById(R.id.button41);
+        btnSound42 = findViewById(R.id.button42);
+        btnSound43 = findViewById(R.id.button43);
+        btnSound44 = findViewById(R.id.button44);
+
 
 
     }
 }
+
+
